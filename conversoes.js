426d3272003362b4b397df498915308d163ea43a
MARCIO FERREIRA DE MOURA <marcio.moura@flexcontact.com.br>
2022-11-30 17:27:16 -0300

    Códigos e conversoes

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de de dado
+// booleanos
+
+//Conversão implicita - permite que a conversão de um tipo de dado em outro
+
+const numero = 456
+const numeroString = "456"
+
+console.log(numero === numeroString) //False
+
+console.log(numero == numeroString) //True
+console.log(numero + numeroString) //Concatena a varial
+//Conversão explicita
+//Number()
+//String()
+
+console.log(numero + Number(numeroString)) //Concatena a varial
+console.log(numero + Number(numeroString))
