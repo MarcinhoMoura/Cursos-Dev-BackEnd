edfdc9a03725c92a295a237d3c5520f9c869c1ac
MARCIO FERREIRA DE MOURA <marcio.moura@flexcontact.com.br>
2022-12-07 11:52:38 -0300

    operadores e funções

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("HelloWorld!");
+const nome = "Leo";
+const idade = 16;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
